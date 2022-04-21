d1bb47dc890de3dc436a553336e02a8906ad96b4
Oscar <oscarballestach@gmail.com>
2022-04-20 21:45:48 -0500

    first commit

---
+++ src/test/java/back/ropa/RopaApplicationTests.java
@@ -0,0 +1,13 @@
+package back.ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
